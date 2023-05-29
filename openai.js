48ab33760d0f2fdeaf66fb5b9c6c72debb215a29
Rastamafugg <jasondeanross@gmail.com>
2023-05-29 20:31:53 -0300

    openai initial code
    Added initial openai and axios code to generate text

--- openai.js
+++ openai.js
@@ -1,6 +1,22 @@
-import { OpenAIAPI } from 'openai';
+import axios from 'axios';
 
-const openai = new OpenAIAPI(process.env.OPENAI_API_KEY);
+const openaiAPI = axios.create({
+    baseURL: 'https://api.openai.com/v1/engines/davinci-codex/completions',
+    timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+    }
+});
 
-export default openai;
-
+export async function generateText(prompt) {
+    try {
+        const response = await openaiAPI.post('', {
+            prompt: prompt,
+            max_tokens: 100
+        });
+        return response.data.choices[0].text.trim();
+    } catch (error) {
+        console.error(error);
+    }
+}
