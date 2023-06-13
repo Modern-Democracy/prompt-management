ab3659800085dd8da818296cac9b51ce75ceb58d
Rastamafugg <jasondeanross@gmail.com>
2023-06-13 12:58:52 -0300

    Latest working code

--- hosting/src/App.svelte
+++ hosting/src/App.svelte
@@ -1,71 +1,59 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from './assets/vite.svg'
-  import Counter from './lib/Counter.svelte'
-  import ChatGPTConversation from "./lib/ChatGPTConversation.svelte";
-  import {onMount} from "svelte";
-  import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-  import {googleAuthProvider} from "./lib/firebase";
-
-  onMount(() => {
-    const auth = getAuth();
-    signInWithPopup(auth, googleAuthProvider)
-            .then((result) => {
-              // This gives you a Google Access Token. You can use it to access the Google API.
-              const credential = GoogleAuthProvider.credentialFromResult(result);
-              const token = credential.accessToken;
-              // The signed-in user info.
-              const user = result.user;
-              // IdP data available using getAdditionalUserInfo(result)
-              // ...
-            }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
-  });
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="drawer">
+    <div class="drawer-toggle"></div>
+    <div class="drawer-content">
+      <div class="navbar bg-base-100">
+        <div class="flex-1">
+          <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+        </div>
+        <div class="flex-none">
+          <ul class="menu menu-horizontal px-1">
+            <li><a>Link</a></li>
+            <li>
+              <details>
+                <summary>
+                  Parent
+                </summary>
+                <ul class="p-2 bg-base-100">
+                  <li><a>Link 1</a></li>
+                  <li><a>Link 2</a></li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="chat">
+        <div class="chat chat-start">
+          <div class="chat-bubble">It's over Anakin, <br/>I have the high ground.</div>
+        </div>
+        <div class="chat chat-end">
+          <div class="chat-bubble">You underestimate my power!</div>
+        </div>
+      </div>
+      <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+        <div>
+          <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+        </div>
+      </footer>
+    </div>
+    <div class="drawer-side">
+      <div class="drawer-overlay">
+      </div>
+      <ul class="menu bg-base-200 w-56 rounded-box">
+        <li><a>Item 1</a></li>
+        <li><a>Item 2</a></li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <div class="card">
-    <ChatGPTConversation />
-  </div>
-
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
 </style>
