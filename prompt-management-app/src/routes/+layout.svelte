3b4bea19be4b32efa47c763a013cc5639c758e70
Rastamafugg <jasondeanross@gmail.com>
2023-06-19 09:45:26 -0300

    - Removed most example code - Moved .env and added .env.example to new application folder - Added firebase library and library code

--- prompt-management-app/src/routes/+layout.svelte
+++ prompt-management-app/src/routes/+layout.svelte
@@ -14,33 +14,30 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase">Prompt Management</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				{#if true}
+					<a
+							class="btn btn-sm variant-ghost-surface"
+							href="login"
+					>
+						Login
+					</a>
+					<a
+							class="btn btn-sm variant-ghost-surface"
+							href="register"
+					>
+						Register
+					</a>
+				{:else}
+					<a
+							class="btn btn-sm variant-ghost-surface"
+							href="logout"
+					>
+						Logout
+					</a>
+				{/if}
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
