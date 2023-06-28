1902cc9b7343d2498486301671c0dfe6f5c21c0b
Rastamafugg <jasondeanross@gmail.com>
2023-06-28 11:06:40 -0300

    - added CostModel to registry

--- prompt-management-app/src/lib/misc/modalComponentRegistry.ts
+++ prompt-management-app/src/lib/misc/modalComponentRegistry.ts
@@ -1,11 +1,13 @@
 import type {ModalComponent} from "@skeletonlabs/skeleton";
 import ContextModal from "$lib/Modals/ContextModal.svelte";
+import CostModal from "$lib/Modals/CostModal.svelte";
 import SettingsModal from "$lib/Modals/SettingsModal.svelte";
 import ShareModal from "$lib/Modals/ShareModal.svelte";
 import SuggestTitleModal from "$lib/Modals/SuggestTitleModal.svelte";
 
 export const modalComponentRegistry: Record<string, ModalComponent> = {
     ContextModal: { ref: ContextModal },
+    CostModal: { ref: CostModal },
     SettingsModal: { ref: SettingsModal },
     ShareModal: { ref: ShareModal },
     SuggestTitleModal: { ref: SuggestTitleModal },
