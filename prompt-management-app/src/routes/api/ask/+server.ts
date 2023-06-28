28f02717614fbd6600d1d0505376dbae73a2311e
Rastamafugg <jasondeanross@gmail.com>
2023-06-28 17:48:13 -0300

    - rewrote POST endpoint to be a simple passthrough for the Chat Completion API call, handling both streaming and non-streaming options

--- prompt-management-app/src/routes/api/ask/+server.ts
+++ prompt-management-app/src/routes/api/ask/+server.ts
@@ -3,6 +3,7 @@ import type { RequestHandler } from './$types';
 import type { OpenAiSettings } from '$lib/misc/openai';
 import { error } from '@sveltejs/kit';
 import { getErrorMessage, throwIfUnset } from '$lib/misc/error';
+import {PUBLIC_OPENAI_API_KEY} from "$env/static/public";
 
 
 export const POST: RequestHandler = async ({ request, fetch }) => {
@@ -10,20 +11,7 @@ export const POST: RequestHandler = async ({ request, fetch }) => {
 		const requestData = await request.json();
 		throwIfUnset('request data', requestData);
 
-		const messages: ChatCompletionRequestMessage[] = requestData.messages;
-		throwIfUnset('messages', messages);
-
-		const settings: OpenAiSettings = requestData.settings;
-		throwIfUnset('settings', settings);
-
-		const openAiKey: string = requestData.openAiKey;
-		throwIfUnset('OpenAI API key', openAiKey);
-
-		const completionOpts: CreateChatCompletionRequest = {
-			...settings,
-			messages,
-			stream: true
-		};
+		const openAiKey: string = PUBLIC_OPENAI_API_KEY;
 
 		// We'll disable the old API for now as it handles stuff quite differently..
 		// OpenAI will probably make old models available for the new API soon.
@@ -34,25 +22,47 @@ export const POST: RequestHandler = async ({ request, fetch }) => {
 		// 		: 'https://api.openai.com/v1/completions';
 		const apiUrl = 'https://api.openai.com/v1/chat/completions';
 
-		const response = await fetch(apiUrl, {
-			headers: {
-				Authorization: `Bearer ${openAiKey}`,
-				'Content-Type': 'application/json'
-			},
-			method: 'POST',
-			body: JSON.stringify(completionOpts)
-		});
+		if (requestData.stream) {
+			const response = await fetch(apiUrl, {
+				headers: {
+					Authorization: `Bearer ${openAiKey}`,
+					'Content-Type': 'application/json'
+				},
+				method: 'POST',
+				body: JSON.stringify(requestData)
+			});
 
-		if (!response.ok) {
-			const err = await response.json();
-			throw err.error;
-		}
-
-		return new Response(response.body, {
-			headers: {
-				'Content-Type': 'text/event-stream'
+			if (!response.ok) {
+				const err = await response.json();
+				throw err.error;
 			}
-		});
+
+			return new Response(response.body, {
+				headers: {
+					'Content-Type': 'text/event-stream'
+				}
+			});
+		} else {
+			const response = await fetch(apiUrl, {
+				headers: {
+					Authorization: `Bearer ${openAiKey}`,
+					'Content-Type': 'application/json'
+				},
+				method: 'POST',
+				body: JSON.stringify(requestData)
+			});
+
+			if (!response.ok) {
+				const err = await response.json();
+				throw err.error;
+			}
+
+			return new Response(response.body, {
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+		}
 	} catch (err) {
 		throw error(500, getErrorMessage(err));
 	}
